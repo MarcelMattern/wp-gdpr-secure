28327b73d607f429c55cfba44a2651849493090a
Marcel Mattern <marcelmattern@googlemail.com>
2019-01-24 21:29:43 +0100

    Add YouTube Replace

--- dsgvo-gdpr-secure.php
+++ dsgvo-gdpr-secure.php
@@ -17,3 +17,10 @@ function gdpr_scripts()
     wp_enqueue_script( 'gdpr-dsgvo-secure');
 }
 add_action( 'wp_enqueue_scripts', 'gdpr_scripts' );
+
+function gdpr_styles()
+{
+    wp_register_style( 'gdpr-dsgvo-secure', plugins_url( '/dsgvo-gdpr-secure/assets/css/gdpr.min.css' ));
+    wp_enqueue_style( 'gdpr-dsgvo-secure');
+}
+add_action( 'wp_enqueue_scripts', 'gdpr_styles' );
